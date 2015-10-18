a88deb9de8a18aaf6fbd5db285c4e08f2367e3fd
Ed McLain <emclain@digitalmotion.co>
2015-10-17 23:21:14 -0500

    More minor chages to setup script

--- mySETUP.sh
+++ mySETUP.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
+
+# Ensure we have everything we need to run properly
+for app in basename dirname expect scp ssh; do
+  command -v ${app} >/dev/null 2>&1 || { echo >&2 "${app} is required for me to run but I cannot find it.  Aborting."; exit 1; }
+done
+
 APP=$(basename $0)
 APPPATH=$(dirname $0)
 LIBDIR="AWS-IoT-Python-Library"
-YUNBASE="/opt/aws-iot/"
+YUNBASE="/opt/aws-iot"
 
+
+# Our help information
 usage() {
   echo "${APP} is used to setup and copy the AWS IoT Python Library to an Arduino Yun"
   echo "${APP} Usage:"
@@ -13,7 +21,6 @@ usage() {
   echo ""
   exit 1
 }
-
 # Make an ssh call to the YUN
 yunssh() {
   ${APPPATH}/utils/yunssh.sh "${YUNIP}" "${YUNPASS}" "${1}"
@@ -31,15 +38,15 @@ install-mqtt() {
 #This sub creates our aws-iot directories on the Yun and copies over the initial certs
 create-directories() {
   yunssh "mkdir -p ${YUNBASE}/bin ${YUNBASE}/certs ${YUNBASE}/lib "
-  yunscp "${LIBPATH}/certs/*" "${YUNBASE}/certs/"
+  yunscp "${APPPATH}/${LIBDIR}/certs/*" "${YUNBASE}/certs/"
 }
 # This sub copies over the aws-iot lib and bin folders
 copy-files() {
-  yunscp "${LIBPATH}/bin/*" "${YUNBASE}/bin/"
-  yunscp "${LIBPATH}/lib/*" "${YUNBASE}/lib/"
+  yunscp "${APPPATH}/${LIBDIR}/bin/*" "${YUNBASE}/bin/"
+  yunscp "${APPPATH}/${LIBDIR}/lib/*" "${YUNBASE}/lib/"
 }
 # Check to make sure we have a valid application and lib directory path available
-if [ ! -d "${APPATH}/${LIBDIR}" ]; then
+if [ ! -d "${APPPATH}/${LIBDIR}" ]; then
   echo "APPPATH not properly set or ${APPPATH}/${LIBDIR} does not exist or is not accessible"
   exit 1
 fi
